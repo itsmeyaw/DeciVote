ccefca95e5ed01da7c07bf778b561858548fb483
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-03-12 22:43:11 +0100

    Added comments and added informative returns

--- contracts/DeciVote.sol
+++ contracts/DeciVote.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.6.12 <0.9.0;
 
 contract DeciVote {
-  address public owner;
+    address public owner;
 
     constructor() {
         owner = msg.sender; // Set the owner as the sender of the contract
@@ -23,40 +23,86 @@ contract DeciVote {
         _;
     }
 
-    function createPoll(string memory _question, string[] memory _choices) public onlyOwner {
+    /** Create a poll, only owner can create a poll
+     *
+     * @param _question The question for the poll
+     * @param _choices Available choices for this poll
+     * @return the index of the poll
+     */
+    function createPoll(
+        string memory _question,
+        string[] memory _choices
+    ) public onlyOwner returns (uint) {
         Poll storage newPoll = polls.push();
         newPoll.question = _question;
         newPoll.choices = _choices;
         newPoll.isOpen = true;
+        return polls.length - 1;
     }
 
+    /** Close a poll, only owner can close a poll
+     *
+     * @param _pollIndex The index of the poll
+     */
     function closePoll(uint _pollIndex) public onlyOwner {
         require(_pollIndex < polls.length, "Poll does not exist.");
         Poll storage poll = polls[_pollIndex];
         poll.isOpen = false;
     }
 
-    function vote(uint _pollIndex, uint _choiceIndex) public {
+    /** Cast a vote for a certain poll
+     *
+     * @param _pollIndex The index of the vote that want to be voted on
+     * @param _choiceIndex The choice of the vote
+     * @return The updated result of the poll
+     */
+    function vote(
+        uint _pollIndex,
+        uint _choiceIndex
+    ) public returns (uint[] memory) {
         require(_pollIndex < polls.length, "Poll does not exist.");
-        require(_choiceIndex < polls[_pollIndex].choices.length, "Invalid choice.");
+        require(
+            _choiceIndex < polls[_pollIndex].choices.length,
+            "Invalid choice."
+        );
         Poll storage poll = polls[_pollIndex];
         require(poll.isOpen, "This poll is closed.");
-        require(!poll.hasVoted[msg.sender], "You have already voted in this poll.");
+        require(
+            !poll.hasVoted[msg.sender],
+            "You have already voted in this poll."
+        );
         poll.hasVoted[msg.sender] = true;
         poll.votes[_choiceIndex]++;
-    }
-
-    function getResults(uint _pollIndex) public view returns (uint[] memory) {
-        require(_pollIndex < polls.length, "Poll does not exist.");
-        Poll storage poll = polls[_pollIndex];
         uint[] memory results = new uint[](poll.choices.length);
-        for(uint i = 0; i < poll.choices.length; i++) {
+        for (uint i = 0; i < poll.choices.length; i++) {
             results[i] = poll.votes[i];
         }
         return results;
     }
 
-    function getPollQuestion(uint _pollIndex) public view returns (string memory) {
+    /** Get the result of a poll
+     *
+     * @param _pollIndex The index of the poll that want to be queried
+     * @return The result of the poll
+     */
+    function getResults(uint _pollIndex) public view returns (uint[] memory) {
+        require(_pollIndex < polls.length, "Poll does not exist.");
+        Poll storage poll = polls[_pollIndex];
+        uint[] memory results = new uint[](poll.choices.length);
+        for (uint i = 0; i < poll.choices.length; i++) {
+            results[i] = poll.votes[i];
+        }
+        return results;
+    }
+
+    /** Get the question of a poll
+     *
+     * @param _pollIndex The index of the poll that want to be queried
+     * @return The question of the poll
+     */
+    function getPollQuestion(
+        uint _pollIndex
+    ) public view returns (string memory) {
         require(_pollIndex < polls.length, "Poll does not exist.");
         return polls[_pollIndex].question;
     }
